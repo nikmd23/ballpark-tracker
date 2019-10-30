1b43deea31648663ceca8ac40e007b4fe2fd6b03
Nik Molnar <nikmd23@gmail.com>
2019-10-30 18:46:31 -0500

    Update postCreate.sh

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -4,8 +4,8 @@ wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add 
 echo "deb http://repo.mongodb.org/apt/debian stretch/mongodb-org/4.2 main" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
 sudo apt-get update
 sudo apt-get install -y mongodb-org
-mkdir -p /data/db /data/configdb
-chown -R mongodb:mongodb /data/db /data/configdb
+sudo mkdir -p /data/db /data/configdb
+sudo chown -R mongodb:mongodb /data/db /data/configdb
 mongod > ~/mongo.log &
 mongoimport --db ballparkTracker --collection parks --file ~/workspace/public/data/ballparks.extjson --drop
 cd ~/workspace
