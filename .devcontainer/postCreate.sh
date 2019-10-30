7d52eb2046c43105e29d5c9b6ff654972385bc24
Nik Molnar <nikmd23@gmail.com>
2019-10-30 17:53:36 -0500

    Create postCreate.sh

---
+++ .devcontainer/postCreate.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
+echo "deb http://repo.mongodb.org/apt/debian stretch/mongodb-org/4.2 main" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
+sudo apt-get update
+sudo apt-get install -y mongodb-org
+mkdir -p /data/db /data/configdb
+chown -R mongodb:mongodb /data/db /data/configdb
+mongod > mongo.log &
