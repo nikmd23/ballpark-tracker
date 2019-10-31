0b65f13edd0b01b53ae0e8ef38e83232d99c34af
Nik Molnar <nikmd23@gmail.com>
2019-10-31 01:33:20 +0000

    Add /api/parks path

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
+var path = require('path');
 var express = require('express');
 var app = express();
-var path = require('path');
+var mongoClient = require('mongodb').MongoClient;
+var mongoUrl = 'mongodb://localhost:27017/';
+var mongoDb = 'ballparkTracker';
+var mongoCollection = 'parks';
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.get('/', (req, res) => res.render('index', {AZURE_MAPS_KEY: process.env.AZURE_MAPS_KEY }));
+
+app.get('/api/parks', (req, res) => {
+    mongoClient.connect(mongoUrl, (err, db) => {
+      var dbo = db.db(mongoDb);
+      var query = { "properties.League": "MLB" };
+
+      dbo.collection(mongoCollection).find(query).toArray(function(err, result) {
+
+        console.log(result);
+        res.json(result);
+        db.close();
+      });
+    });
+
+});
 
 // Configuring static assets (css/js)
 app.use(express.static('public'))
