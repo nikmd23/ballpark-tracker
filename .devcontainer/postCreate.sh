026af440a8c804b80a2346929a1fcba42133583f
Nik Molnar <nikmd23@gmail.com>
2020-05-04 14:46:58 -0500

    Update postCreate.sh

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -1,19 +1,5 @@
 #!/bin/bash
 
-# wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
-# echo "deb http://repo.mongodb.org/apt/debian stretch/mongodb-org/4.2 main" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
-# sudo apt-get update
-# sudo apt-get install -y mongodb-org
-# sudo mkdir -p /data/db /data/configdb
-# sudo chown -R mongodb:mongodb /data/db /data/configdb
-# sudo mongod > ~/mongo.log &
-sudo apt-get update
-sudo apt-get install gnupg
-wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
-echo "deb http://repo.mongodb.org/apt/debian stretch/mongodb-org/4.2 main" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
-sudo apt-get update
-sudo apt-get install mongodb-org-tools
 mongoimport --host "mongo:27017" --db ballparkTracker --collection parks --file $PWD/public/data/ballparks.extjson --drop
 
-npm i -g nodemon
 npm i
