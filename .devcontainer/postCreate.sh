0b44479b2a1b4d6720a9df8c683ec9bc3c798007
Nik Molnar <nikmd23@gmail.com>
2020-05-08 11:17:36 -0500

    Update postCreate.sh

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
-mongoimport --host "mongo:27017" --db ballparkTracker --collection parks --file $PWD/public/data/ballparks.extjson --drop
+SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
+
+echo SCRIPT PATH
+echo $SCRIPTPATH
+
+mongoimport --host "mongo:27017" --db ballparkTracker --collection parks --file $SCRIPTPATH/../public/data/ballparks.extjson --drop
 
 npm i
