5394d249d08b7daa9071a8a3c8c18bfbb9d9dcdf
Nik Molnar <nimolnar@microsoft.com>
2020-05-09 14:38:33 +0000

    Prep for demo

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 // app.use(express.json());
 
-// main app page
+//main app page
 app.get('/', (req, res) => res.render('index', { AZURE_MAPS_KEY: process.env.AZURE_MAPS_KEY }));
 
 // return a json list of ballparks
